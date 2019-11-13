92eb45c4fb70f5969bdf48e63fb92f64c76c8653
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-11-13 12:48:16 +0100

    Add project files.

---
+++ LibreriaArray/GestioneArray.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace LibreriaArray
+{
+    public class GestioneArray
+    {
+        public static void LeggiArray(int[] array)
+        {
+
+        }
+        public static void StampaArray(int[] array)
+        {
+
+        }
+        public static int[] CopiaArray(int[] array)
+        {
+
+        }
+        public static bool RicercaArray(int[] array, int elemento)
+        {
+
+        }
+        public static bool SonoUguali(int[] array1, int[] array2)
+        {
+
+        }
+    }
+}
